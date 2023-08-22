efade9da443f742fabfb7127942220f225d35993
frankiejun <frankiejun@qq.com>
2023-08-22 23:10:15 +0800

    k8s中运行webook实例

---
+++ week3/webook/internal/repository/dao/user.go
@@ -0,0 +1,80 @@
+package dao
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/go-sql-driver/mysql"
+	"gorm.io/gorm"
+	"time"
+)
+
+// ErrDataNotFound 通用的数据没找到
+var ErrDataNotFound = gorm.ErrRecordNotFound
+
+// ErrUserDuplicate 这个算是 user 专属的
+var ErrUserDuplicate = errors.New("用户邮箱或者手机号冲突")
+
+type UserDAO interface {
+	Insert(ctx context.Context, u User) error
+	FindByPhone(ctx context.Context, phone string) (User, error)
+	FindByEmail(ctx context.Context, email string) (User, error)
+	FindById(ctx context.Context, id int64) (User, error)
+}
+
+type GORMUserDAO struct {
+	db *gorm.DB
+}
+
+func NewGORMUserDAO(db *gorm.DB) *GORMUserDAO {
+	return &GORMUserDAO{
+		db: db,
+	}
+}
+
+func (ud *GORMUserDAO) Insert(ctx context.Context, u User) error {
+	now := time.Now().UnixMilli()
+	u.Ctime = now
+	u.Utime = now
+	err := ud.db.WithContext(ctx).Create(&u).Error
+	if me, ok := err.(*mysql.MySQLError); ok {
+		const uniqueIndexErrNo uint16 = 1062
+		if me.Number == uniqueIndexErrNo {
+			return ErrUserDuplicate
+		}
+	}
+	return err
+}
+
+func (ud *GORMUserDAO) FindByPhone(ctx context.Context, phone string) (User, error) {
+	var u User
+	err := ud.db.WithContext(ctx).First(&u, "phone = ?", phone).Error
+	return u, err
+}
+
+func (ud *GORMUserDAO) FindByEmail(ctx context.Context, email string) (User, error) {
+	var u User
+	err := ud.db.WithContext(ctx).First(&u, "email = ?", email).Error
+	return u, err
+}
+
+func (ud *GORMUserDAO) FindById(ctx context.Context, id int64) (User, error) {
+	var u User
+	err := ud.db.WithContext(ctx).First(&u, "id = ?", id).Error
+	return u, err
+}
+
+type User struct {
+	Id int64 `gorm:"primaryKey,autoIncrement"`
+	// 设置为唯一索引
+	Email    sql.NullString `gorm:"unique"`
+	Password string
+
+	//Phone *string
+	Phone sql.NullString `gorm:"unique"`
+
+	// 创建时间
+	Ctime int64
+	// 更新时间
+	Utime int64
+}
---
+++ week3/webook/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	//server := gin.Default()
+	server := InitWebServer()
+	// 注册路由
+	server.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello, world")
+	})
+	server.Run(":8081")
+}
